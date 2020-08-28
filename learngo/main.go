fad5ff1b10e94fdbc5f19c345ba65209c12d46a7
XxCtrlZxX <harryjung1001@gmail.com>
2020-08-28 22:25:10 +0900

    lecture 1.4

--- learngo/main.go
+++ learngo/main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-func repeatMe(words ...string) {
-	fmt.Println(words)
+// several returns
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+// naked return
+func lenAndUpper2(name string) (length int, uppercase string) {
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	repeatMe("a", "b", "c", "d")
+	len, up := lenAndUpper("abcd")
+	fmt.Println(len, up)
+	length, upper := lenAndUpper2("efghijk")
+	fmt.Println(length, upper)
 }
