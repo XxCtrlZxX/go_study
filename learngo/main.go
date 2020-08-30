cd5af2c6aae1a07782edab0ff8484f835669861e
XxCtrlZxX <harryjung1001@gmail.com>
2020-08-30 23:26:02 +0900

    lecture 1.5

--- learngo/main.go
+++ learngo/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-// naked return
-func lenAndUpper2(name string) (length int, uppercase string) {
+// 1.4 naked return
+/*func lenAndUpper2(name string) (length int, uppercase string) {
 	defer fmt.Println("I'm dome") // defer : 함수가 끝났을 때 실행
 	length = len(name)
 	uppercase = strings.ToUpper(name)
@@ -16,4 +15,18 @@ func lenAndUpper2(name string) (length int, uppercase string) {
 func main() {
 	length, upper := lenAndUpper2("abcde")
 	fmt.Println(length, upper)
+}*/
+
+// 1.5 for
+func superAdd(numbers ...int) int {
+	var total int = 0
+	for _, num := range numbers {
+		total += num
+	}
+	return total
+}
+
+func main() {
+	result := superAdd(1, 2, 3, 4, 5, 6)
+	fmt.Println(result)
 }
