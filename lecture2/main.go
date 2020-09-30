ceab72020e17625888c63fff32a6404ba6cb5d8c
XxCtrlZxX <harryjung1001@gmail.com>
2020-09-30 13:48:14 +0900

    lecture 2.2 Methods

--- lecture2/main.go
+++ lecture2/main.go
@@ -8,5 +8,12 @@ import (
 
 func main() {
 	account := accounts.NewAccount("CtrlZ")
-	fmt.Println(account)
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
 }
